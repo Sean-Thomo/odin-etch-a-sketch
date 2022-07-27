f4a70db4df864822e6dae50235498b5ad3fe59e5
siphesihle-thomo <seansthomo@gmail.com>
2022-07-27 18:08:05 +0200

    Add btn style and random color generator

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-
-
 function createGrid(gridSize) {
   const grid = document.getElementById('grid');
-  const cells = grid.querySelectorAll('div');
+  // const cells = grid.querySelectorAll('div');
 
   grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
   grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
@@ -10,15 +8,29 @@ function createGrid(gridSize) {
   for (let i = 0; i < (gridSize * gridSize); i++) {
     let cell = document.createElement('div');
     cell.addEventListener('mouseover', () => {
-      cell.style.backgroundColor = 'white';
+      cell.style.backgroundColor = 'black';
     })
-    cell.style.backgroundColor = 'black';
+    cell.style.backgroundColor = 'grey';
     grid.insertAdjacentElement('beforeend', cell)
   }
 }
 
+function changeColor( color) {
+  if (color == 'random'){
+    this.style.backgroundColor = randomColor();
+  } else {
+    this.style.backgroundColor = color;
+  }
+}
+
+function randomColor() {
+  return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
+}
+
 function app() {
-  createGrid(3);
+  createGrid(90);
 }
 
 window.onload = app();
+
+
